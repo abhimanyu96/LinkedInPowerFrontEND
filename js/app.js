629bc0ff0a0bf5a3183416ca0e4890a56cf04556
Abhimanyu Chopra <abhi.livemail@gmail.com>
2016-07-24 04:18:21 +0530

    Second commit

--- js/app.js
+++ js/app.js
@@ -2,12 +2,21 @@
 
 var app=angular.module('login', []);
 app.controller('loginController',['$http', function($http){
-	this.status={};
+	this.status='';
 	this.data={};
+	var s=this;
+	this.token='';;
 	this.login=function()
 	{
-		$http.post('http://localhost:8080', this.data )
-		.then(this.status='Success',this.status='inValid');
+		$http.post('http://localhost:8080', s.data )
+		.then(function successCallback(response){
+			console.log('sjdnsjd');
+			s.status=response.statusText;
+			s.token=response.data.token;
+			}
+			,function errorCallback(response){
+				console.log('jndjnsd' + response);
+			 s.status='Invalid Username or Password';});
 	};
 }]);
 })();
